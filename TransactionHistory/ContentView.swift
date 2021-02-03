fa4ba6c1db6c69e5f67e6f3d352ca74126cbae97
Akin <rapiddevelopa@gmail.com>
2021-02-03 17:33:55 +0100

    Initial Commit

---
+++ TransactionHistory/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TransactionHistory
+//
+//  Created by Admin on 2/3/21.
+//  Copyright © 2021 rapid interactive. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
